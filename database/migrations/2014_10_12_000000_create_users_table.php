6ead00bcac19e38a4278bdbb44a3c4331ab74ed7
Tahmid <tahmid@pop-os.localdomain>
2022-12-19 14:05:17 +0600

    migration of user table updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('adress')->nullable();
+            $table->string('phone')->unique();
+
+            $table->string('role_1')->default('f');
+            $table->string('role_2')->nullable();
+            
             $table->rememberToken();
             $table->timestamps();
         });
