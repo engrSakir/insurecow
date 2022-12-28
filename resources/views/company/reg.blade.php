f509ff902a5a58d6216344aaab0dccf65c8eb5e7
Mithun <msovik399@gmail.com>
2022-12-28 15:19:45 +0600

    sweetalert add,company&superadmin delete portion change,pending policy & quotations add

--- resources/views/company/reg.blade.php
+++ resources/views/company/reg.blade.php
@@ -14,6 +14,7 @@
                   @foreach ($errors->all() as $error)
                       <li>{{ $error }}</li>
                   @endforeach
+                  
               </ul>
           </div>
       @endif
