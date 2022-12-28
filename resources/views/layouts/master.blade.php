c7e1159d4fc06f1901f51f1f80a4a4c9469f7e92
Mithun <msovik399@gmail.com>
2022-12-28 15:37:31 +0600

    sweetalert added in farmer reg_one and farmer reg_one validation

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -34,7 +34,7 @@
 
  <!--==================== JavaScript Bundle with Popper ====================-->
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
- integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+ integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script class="jsbin" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 
 //=========form js===============
@@ -44,6 +44,8 @@
 <script src="{{asset('js/reg_js/special_marks.js')}}"></script>
 <script src="{{asset('js/reg_js/cow_with_owner.js')}}"></script>
 <script src="{{asset('js/reg_js/pdf_upload.js')}}"></script>
-</script>
+
+
+
 </body>
 </html>
