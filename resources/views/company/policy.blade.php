dd2f4e73c7d11a7adf049c3ec3de64b543646457
Mithun <msovik399@gmail.com>
2023-01-11 17:16:13 +0600

    fix

--- resources/views/company/policy.blade.php
+++ resources/views/company/policy.blade.php
@@ -8,32 +8,37 @@
         <form action="{{ route('policy.store') }}" method="post" enctype="multipart/form-data">
 
             {{ csrf_field() }}
+            {{--            <div class="mb-3">--}}
+            {{--                <label for="exampleFormControlInput1" class="form-label">Logo</label>--}}
+            {{--                <input type="file" name="logo" class="form-control">--}}
+            {{--            </div>--}}
 
             <div class="mb-3">
                 <label for="exampleFormControlTextarea1" class="form-label">Policy</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="content" value="{{ old('content') }}" required></textarea>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="content" value="{{ old('content') }}" requried></textarea>
             </div>
 
             @error('content')
             <div class="alert alert-danger" style="margin-top: 10px">{{ $message }}</div>
             @enderror
             <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Corona Virus Exclusion</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="corona" value="{{ old('corona') }}" required></textarea>
+                <label for="exampleFormControlTextarea1" class="form-label">Corona Exclusion</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="corona" value="{{ old('corona') }}" requried></textarea>
             </div>
             @error('content')
             <div class="alert alert-danger" style="margin-top: 10px">{{ $message }}</div>
             @enderror
             <div class="mb-3">
                 <label for="exampleFormControlTextarea1" class="form-label">Schedule</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="schedule" value="{{ old('schedule') }}" required></textarea>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="schedule" value="{{ old('schedule') }}" requried></textarea>
             </div>
             @error('content')
             <div class="alert alert-danger" style="margin-top: 10px">{{ $message }}</div>
             @enderror
+
             <div class="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">Signature</label>
-                <input type="file" name="signature" class="form-control">
+                <input type="file" name="signature" class="form-control" value="{{ old('signature') }}" requried>
             </div>
 
             @error('signature')
--- resources/views/layouts/company.blade.php
+++ resources/views/layouts/company.blade.php
@@ -194,10 +194,10 @@
     <!-- Page level custom scripts -->
 
 
-    <script src="{{asset('vendor/chart.js/Chart.min.js')}}"></script>
+{{--    <script src="{{asset('vendor/chart.js/Chart.min.js')}}"></script>--}}
 
-    <script src="{{asset('js/demo/chart-area-demo.js')}}"></script>
-    <script src="{{asset('js/demo/chart-pie-demo.js')}}"></script>
+{{--    <script src="{{asset('js/demo/chart-area-demo.js')}}"></script>--}}
+{{--    <script src="{{asset('js/demo/chart-pie-demo.js')}}"></script>--}}
 
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
