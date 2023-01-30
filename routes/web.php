03ab11fd866a2eb4ee31525262a2f1542bd4bf86
unknown <ahsanrafin12@gmail.com>
2023-01-30 14:15:29 +0600

    UPDATED

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ use App\Http\Controllers\CompanyController;
 */
 
 Route::get('/', function () {
-    return view('landing');
-//    return redirect()->route('farmer.login');
+    // return view('landing');
+   return redirect()->route('farmer.login');
 });
 
 Auth::routes();
